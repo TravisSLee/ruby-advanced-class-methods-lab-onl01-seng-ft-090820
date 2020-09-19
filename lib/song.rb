70f70fdcedea91503d010eaa54f66fd5cd36e534
Travis Lee <travisslee223@gmail.com>
2020-09-19 18:23:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,39 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+  
+  def self.new_by_name(title) 
+    song = self.new
+    song.name = title
+    song
+  end
+
+  def self.create_by_name(title) 
+    song = self.create
+    song.name = title
+    song
+  end
+  
+  def self.find_by_name(title)
+     @@all.find{|song| song.name == title}
+  end
+  
+  def self.find_or_create_by_name(title)
+    result = self.find_by_name(title)
+      if result
+        return result
+    else
+      self.create_by_name(title)
+    end
+  end
+  def self.aplhabetical
+    self.all.sort 
+  end
 
 end
